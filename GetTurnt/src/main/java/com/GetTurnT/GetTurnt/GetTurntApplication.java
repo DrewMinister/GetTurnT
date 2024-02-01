e07c86682db945cecd03c1907d00350cc3cf53fa
Drew Minister <148986868+DrewMinister@users.noreply.github.com>
2024-02-01 18:26:08 -0500

    Add files via upload

---
+++ GetTurnt/src/main/java/com/GetTurnT/GetTurnt/GetTurntApplication.java
@@ -0,0 +1,13 @@
+package com.GetTurnT.GetTurnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetTurntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetTurntApplication.class, args);
+	}
+
+}
---
+++ GetTurnt/src/test/java/com/GetTurnT/GetTurnt/GetTurntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GetTurnT.GetTurnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTurntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
